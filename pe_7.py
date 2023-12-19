af8b18ef14800270943ce7a8dc1f95e5d2985529
benji <you@example.com>
2023-12-18 21:46:51 -0800

    first problem

---
+++ pe_7.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def find_10001st_prime():
+    count = 0
+    n = 1
+    while True:
+        if is_prime(n):
+            count += 1
+        if count == 10001:
+            return n
+        n += 1
+
+result = find_10001st_prime()
+print(result)
